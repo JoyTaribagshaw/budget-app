764da606c3b538bcffa5e9341f85b0fe94b467f7
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-11-21 13:35:25 +0000

    fix rubocop error

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 class SplashController < ApplicationController
-    def index
-      @app_name = 'Joy Budget App'
-      @tagline = 'Track and manage your expenses effortlessly.'
-      @cta_signup = 'Sign Up'
-      @cta_login = 'Log In'
-    end
+  def index
+    @app_name = 'Joy Budget App'
+    @tagline = 'Track and manage your expenses effortlessly.'
+    @cta_signup = 'Sign Up'
+    @cta_login = 'Log In'
+  end
 end
-  

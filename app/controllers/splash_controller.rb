db13c852514bd47f541d3173d500bd0d5c08b46d
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-11-21 13:14:03 +0000

    create simple page with the name of my app

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,4 +1,9 @@
 class SplashController < ApplicationController
     def index
+      @app_name = 'Your Budget App'
+      @tagline = 'Track and manage your expenses effortlessly.'
+      @cta_signup = 'Sign Up'
+      @cta_login = 'Log In'
     end
 end
+  

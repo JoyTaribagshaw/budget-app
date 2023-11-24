2097cb200efaa761bda10dc37a285acb8e5fbaa2
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-11-24 19:44:02 +0000

    fix linter errors

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,8 @@
 class Category < ApplicationRecord
-    belongs_to :user
-    has_many :category_expenditures, dependent: :destroy
-    has_many :expenditures, through: :category_expenditures
-    has_one_attached :icon
-  
-    validates :name, presence: true
+  belongs_to :user
+  has_many :category_expenditures, dependent: :destroy
+  has_many :expenditures, through: :category_expenditures
+  has_one_attached :icon
+
+  validates :name, presence: true
 end

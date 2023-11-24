e143fb397a42037cf18742ff73d4f0ad12a5c1be
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-11-24 14:04:59 +0000

    update controller

--- app/controllers/expenditures_controller.rb
+++ app/controllers/expenditures_controller.rb
@@ -1,2 +1,33 @@
 class ExpendituresController < ApplicationController
-end
+    before_action :authenticate_user!
+    before_action :set_category
+  
+    def index
+      @expenditures = @category.expenditures.order(created_at: :desc)
+    end
+  
+    def new
+      @expenditure = @category.expenditures.new
+    end
+  
+    def create
+      @expenditure = @category.expenditures.new(expenditure_params)
+  
+      if @expenditure.save
+        redirect_to category_expenditures_path(@category), notice: "expenditure created successfully."
+      else
+        render :new
+      end
+    end
+  
+    private
+  
+    def set_category
+      @category = category.find(params[:category_id])
+    end
+  
+    def expenditure_params
+      params.require(:expenditure).permit(:amount)
+    end
+  end
+  
